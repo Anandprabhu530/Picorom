ae6a95a721bfd0f70af7aac0d31b3009e739ffb4
Anandprabhu530 <anandprabhu530@gmail.com>
2024-12-16 17:16:56 +0530

    Almost done with cloud run code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 import sharp from "sharp";
-import {delete_file, downloadFromBucket} from "./utils";
+import {delete_file, downloadFromBucket, uploadToBucket} from "./utils";
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.get("/transform", async (req, res) => {
   const trimmedFileName = fileName.split(".");
 
   //download video from google storage
-  // await downloadFromBucket(fileName);
+  await downloadFromBucket(fileName);
 
   const inputFilePath = path.resolve(__dirname, ".", "inputDir", fileName);
 
@@ -22,14 +22,27 @@ app.get("/transform", async (req, res) => {
     "outputDir",
     `${trimmedFileName[0]}.${coversionType}`
   );
+  try {
+    // convert filetype
+    const data = await sharp(inputFilePath)
+      .toFormat(coversionType)
+      .toFile(outputFilePath);
 
-  const data = await sharp(inputFilePath)
-    .toFormat(coversionType)
-    .toFile(outputFilePath);
+    //upload file to Bucket
+    await uploadToBucket(`trimmedFileName[0].${coversionType}`);
 
-  await delete_file(inputFilePath);
-  await delete_file(`${trimmedFileName[0]}.${coversionType}`);
-  res.status(200).json({server: data});
+    // clean up files
+    await delete_file(`./inputDir/${fileName}`);
+    await delete_file(`./outputDir/${trimmedFileName[0]}.${coversionType}`);
+
+    res.status(200).json({message: data});
+  } catch (error) {
+    // clean up files
+    await delete_file(`./inputDir/${fileName}`);
+    await delete_file(`./outputDir/${trimmedFileName[0]}.${coversionType}`);
+
+    res.status(200).json({message: "Not converted"});
+  }
 });
 
 app.listen(6969);
--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,11 +1,40 @@
 import fs from "fs";
+import path from "path";
+import {Storage} from "@google-cloud/storage";
 
-export async function downloadFromBucket(fileName: string) {}
+const storage = new Storage();
+
+const inputBucket = "input-bucket";
+const outputBucket = "output-bucket";
+
+const inputDir = "./inputDir";
+const outputDir = "./outputDir";
+
+export async function downloadFromBucket(fileName: string) {
+  try {
+    await storage
+      .bucket(inputBucket)
+      .file(fileName)
+      .download({destination: `${inputDir}/${fileName}`});
+  } catch (error) {
+    console.log(`Error Occured - ${error}`);
+  }
+}
+
+export async function uploadToBucket(fileName: string) {
+  const sourcePath = `${outputDir}/${fileName}`;
+  await storage
+    .bucket(outputBucket)
+    .upload(sourcePath, {destination: fileName});
+  console.log(`Processed video uploaded to bucket: ${fileName}`);
+}
 
 export async function delete_file(fileName: string) {
+  const currentDir = path.resolve(__dirname, fileName);
+
   return new Promise<void>((resolve, reject) => {
-    if (fs.existsSync(fileName)) {
-      fs.unlink(fileName, (error) => {
+    if (fs.existsSync(currentDir)) {
+      fs.unlink(currentDir, (error) => {
         if (error) {
           console.log(`An error occured ${error?.message}`);
           reject(error);
